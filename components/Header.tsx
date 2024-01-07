9ef5e20db0cefec98494374fe98e84de040f9b31
Aristine-ig <aristine277@gmail.com>
2024-01-07 22:35:09 +0530

    hi3

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,8 @@ const Header: React.FC<HeaderProps> = ({
       )}>
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button 
+          <button
+            
             onClick={() => router.back()} 
             className="
               rounded-full 
